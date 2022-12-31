7086d3673e3198939e622d47eeb7c001bcae373b
Aarav Narayan <aarav24@localhost-live.attlocal.net>
2022-12-31 18:24:20 -0500

    adding firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ export default {
     '@nuxtjs/emotion',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/firebase'
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
@@ -45,4 +46,17 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  firebase: {
+    config: {
+          apiKey: "AIzaSyBHA8h7jHepIEqMNuHOAEW5dOO6jID3mtQ",
+          authDomain: "conlang-creator-9bd5e.firebaseapp.com",
+          projectId: "conlang-creator-9bd5e",
+          storageBucket: "conlang-creator-9bd5e.appspot.com",
+          messagingSenderId: "975571533700",
+          appId: "1:975571533700:web:b39dec13c9cc47291127d1"
+        },
+        services: {
+          auth: true // Just as example. Can be any other service.
+        }
+  }
 }
